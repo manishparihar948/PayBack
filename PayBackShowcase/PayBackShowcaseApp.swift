72af10efb169e37009ad9709ded51c892a0cca4f
manishparihar948 <manish.parihar948@gmail.com>
2023-06-27 01:05:11 +0200

    first commit

---
+++ PayBackShowcase/PayBackShowcaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  PayBackShowcaseApp.swift
+//  PayBackShowcase
+//
+//  Created by Manish Parihar on 27.06.23.
+//
+
+import SwiftUI
+
+@main
+struct PayBackShowcaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
